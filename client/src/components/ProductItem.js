cc0da0bd70ef4359d88c184a01b784b6506d4a83
NellyChen <77566624+Huajuan337@users.noreply.github.com>
2023-04-03 22:41:04 -0700

    finish home and detail page front-end

--- client/src/components/ProductItem.js
+++ client/src/components/ProductItem.js
@@ -1,17 +1,52 @@
 import React from "react";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
+import { useNavigate } from 'react-router';
 
 const ProductItem = (
     {
         item = {
             "id": "p111111",
-            "name": "item"
+            "name": "item", 
+            "price": 10.00,
+            "description": "While Bootstrap uses ems or rems for defining most sizes, pxs are used for grid breakpoints and container width.",
         }
     }
 ) => {
+    let navigate = useNavigate(); 
     return (
-        <div>
-            <Link to={`/details/${item.id}`} className="btn btn-link">{item.name}</Link>
+        // <li className="list-group-item"
+        //     onClick={() => navigate(`/details/${item.id}`, { state: item })}>
+        //     <div className="row">
+        //         <div className="col-6">
+        //             Name: {item.name}<br/>
+        //             Price: <i class="bi bi-currency-dollar"></i> {item.price}<br/>
+        //             Description:<p className="text-secondary">{item.description}</p>
+                    
+        //         </div>
+        //         <div className="col-6">
+        //             <img className="float-end rounded" src={`/image/${item.image}`} alt='' width='50%'/>
+        //         </div>
+
+        //     </div>
+        // </li>
+
+        <div className="col-sm-4 mb-3">
+            <div className="card p-2">
+                <div onClick={() => navigate(`/details/${item.id}`, { state: item })}>
+                    <img className="rounded" src={`/image/${item.image}`} alt='' width='100%'/>
+                    <span className="text-secondary">{item.name}</span>
+                    <p className="fw-bold">${item.price}</p>  
+                </div>
+                <div className="row justify-content-between mb-2">
+                    <button type="button" className="col-5 btn btn-primary btn-sm ms-3"
+                        onClick={()=>{console.log("add button")}}>
+                        Add
+                    </button>
+                    <button type="button" className="col-5 btn btn-sm btn-primary me-3">
+                        Edit
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
--- client/src/details/DetailsPage.js
+++ client/src/details/DetailsPage.js
@@ -1,13 +1,37 @@
 import React from "react";
-import {useParams} from "react-router";
+import {useLocation, useParams} from "react-router";
 import BackButtonComponent from "../components/BackButtonComponent";
 
 const DetailsPage = () => {
-    const params = useParams();
+    // const params = useParams();
+    const { state } = useLocation();
+    
     return (
         <div>
             <BackButtonComponent/>
-            Detail page for item {params.detailsId};
+            {/* Detail page for item {params.detailsId}; */}
+            <h2>Product Detail</h2>
+           
+                <div className="row">
+                    <div className="col-7">
+                        <img className="rounded" src={`/image/${state.image}`} alt='' width='100%'/>
+                    </div>
+                    <div className="col-4 ms-4">
+                        <h3 className="fw-bold text-secondary">{state.name}</h3>
+                        <h3 className="fw-bold">${state.price}</h3><br/>
+                        <p className="text-secondary">{state.description}</p>
+
+                        <div className="row justify-content-between mb-2">
+                            <button type="button" className="col-5 btn btn-primary btn-sm ms-2"
+                                onClick={()=>{console.log("add button")}}>
+                                Add To Cart
+                            </button>
+                            <button type="button" className="col-5 btn btn-sm btn-primary">
+                                Edit
+                            </button>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
