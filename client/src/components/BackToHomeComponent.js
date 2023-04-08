6d3026855e73fda16f150a78e3b1d34bda604512
guanchang98 <guanchangst@163.com>
2023-04-07 22:41:05 -0700

    Profile page draft

---
+++ client/src/components/BackToHomeComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link} from "react-router-dom";
+const BackButtonComponent = () => {
+    return (
+        <div>
+            <Link to="../../home" className="btn btn-link">Back Home</Link><br/>
+        </div>
+
+    )
+}
+
+export default BackButtonComponent;
--- client/src/profile/ProfilePage.js
+++ client/src/profile/ProfilePage.js
@@ -3,35 +3,102 @@ import {useParams} from "react-router";
 import {Link} from "react-router-dom";
 import ProductsList from "../components/ProductsList";
 import BackButtonComponent from "../components/BackButtonComponent";
+import BackToHomeComponent from "../components/BackToHomeComponent";
 import {useLocation} from "react-router";
+import bannerPic from "../images/profile_banner.jpeg";
+import avatar from "../images/avatar_man.png";
+import '../index.css';
 
 const ProfilePage = (props) => {
     const params = useParams();
     const {pathname} = useLocation();
     const paths = pathname.split("/");
+    const userProfile = {
+        bannerPicture: bannerPic,
+        bio: "A test user",
+        dateJoined: "2023-04-05",
+        dateOfBirth: "1998-10-03",
+        firstName: "Chang",
+        lastName: "Guan",
+        followersCount: 3,
+        followingCount: 3,
+        handle: "@changguan",
+        location: "Sunnyvale",
+        profilePicture: avatar,
+        website: "google.com",
+    }
+    const {
+        bannerPicture,
+        bio,
+        dateJoined,
+        dateOfBirth,
+        firstName,
+        followersCount,
+        followingCount,
+        handle,
+        lastName,
+        location,
+        profilePicture,
+    } = userProfile;
     return (
-        <div>
+        <div className="border-1">
             {paths.length > 2 && <BackButtonComponent/>}
-            <h1>ProfilePage</h1>
-            {props.userID && <span>Welcome, user {props.userID}!</span>}
-            {!props.userID && <span>user {params.profileId}'s profile</span>}
-            <ProductsList/>
-            <div className="container">
-                <div className="list-group list-group-horizontal mb-3">
-                    <Link
-                        to="/profile/following"
-                        className={`list-group-item`}
-                    >
-                        3 Following
-                    </Link>
-                    <Link
-                        to="/profile/followers"
-                        className={`list-group-item`}
-                    >
-                        3 Followers
+            {paths.length <= 2 && <BackToHomeComponent/>}
+            {/*{props.userID && <span>Welcome, user {props.userID}!</span>}*/}
+            {/*{!props.userID && <span>user {params.profileId}'s profile</span>}*/}
+
+            <div className="row">
+                <img src={bannerPicture} className="w-100 mb-3" height="240"/>
+                <div className="col-9 float-start">
+                    <img src={profilePicture} className="w-25 wd-pos-absolute-profile-banner"/>
+                </div>
+                <div className="col-3 mb-4">
+                    <Link className="btn btn-primary rounded-3 float-end" to="../edit-profile">
+                        Edit Profile
                     </Link>
                 </div>
+                <div>
+                    <div className="mt-3">
+                        <h2>{firstName} {lastName}</h2>
+                    </div>
+                    <div className="mb-3 text-secondary">
+                        {handle}
+                    </div>
+                    <p>
+                        {bio}
+                    </p>
+                    <div className="row mb-3">
+                        <div className="col-3">
+                            <i className="bi bi-geo-alt text-secondary"></i>
+                            <span className="text-secondary"> {location} </span>
+                        </div>
+                        <div className="col-4">
+                            <i className="bi bi-balloon text-secondary"></i>
+                            <span className="text-secondary"> Born on {dateOfBirth} </span>
+                        </div>
+                        <div className="col-4">
+                            <i className="bi bi-calendar3 text-secondary"></i>
+                            <span className="text-secondary"> Joined {dateJoined} </span>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="mb-3 list-group list-group-horizontal col-5">
+                            <Link to="/profile/following" className="list-group-item list-group-item-action border-0">
+                                <b>{followingCount} </b>
+                                <span className="text-secondary">Following</span>
+                            </Link>
+                            <Link to="/profile/followers" className="list-group-item list-group-item-action border-0">
+                                <b> {followersCount} </b>
+                                <span className="text-secondary">Followers</span>
+                            </Link>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>History</h3>
+                    </div>
+                </div>
             </div>
+            <ProductsList/>
         </div>
 
     )
