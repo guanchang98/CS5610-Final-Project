9d47ca9fc010829ec4d90c54ddedd24a38d076cb
zhugelijuan123 <nancyrain2015@163.com>
2023-04-11 21:34:17 -0700

    check register function

--- client/src/screens/RegisterScreen.js
+++ client/src/screens/RegisterScreen.js
@@ -17,7 +17,7 @@ function RegisterScreen() {
         }
     else {
     try {
-          dispatch(registerThunk({ username, password}));
+          dispatch(registerThunk({ username:username, password:password}));
           navigate("/profile");
         } catch (err) {
           console.log(err);
