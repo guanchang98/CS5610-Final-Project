3d7078005b146e85007652068a23a826ac26f94e
zhugelijuan123 <nancyrain2015@163.com>
2023-04-13 18:34:45 -0700

    delete login html undefined className

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -1,71 +1,58 @@
 import {Link} from "react-router-dom";
-import React, { useState ,useEffect} from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router";
-import { loginThunk } from "../services/users/users-thunks";
+import React, {useState, useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router";
+import {loginThunk} from "../services/users/users-thunks";
 
 
 const LoginScreen = () => {
-    const { currentUser } = useSelector((state) => state.users);
+    const {currentUser} = useSelector((state) => state.users);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const login = async () => {
         try {
-          const user = await dispatch(loginThunk({ username:username, password:password }));
-          console.log("login")
-          console.log(user)
-          navigate("/profile");
+            const user = await dispatch(loginThunk({username, password}));
+            console.log("login")
+            console.log(user)
+            navigate("/profile");
         } catch (err) {
-          console.log(err);
-          alert("wrong user");
+            console.log(err);
         }
     };
     return (
-        <div className="Auth-form-container">
-            <form className="Auth-form">
-                <div className="Auth-form-content">
-                    <h3 className="Auth-form-title">Sign In</h3>
-                    <div className="form-group mt-3">
-                        <label>User Name</label>
-                        <input
-                              type="text"
-                              className="form-control"
-                              value={username}
-                              onChange={(e) => {
-                                setUsername(e.target.value);
-                              }}
-                        />
-                    </div>
-                    <div className="form-group mt-3">
-                        <label>Password</label>
-                        <input
-                              type="password"
-                              className="form-control"
-                              value={password}
-                              onChange={(e) => {
-                                setPassword(e.target.value);
-                              }}
-                        />
-                    </div>
+        <div>
+            <h3>Sign In</h3>
+            <div className="form-group mt-3">
+                <label>User Name</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    value={username}
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                />
+            </div>
+            <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                    type="password"
+                    className="form-control"
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                />
+            </div>
 
-                    <div className="d-grid gap-2 mt-3">
-                        <button onClick={login} className="btn btn-primary">
-                            Login
-                        </button>
+            <div className="d-grid gap-2 mt-3">
+                <button onClick={login} className="btn btn-primary">
+                    Login
+                </button>
 
-                    </div>
-                    <div>
-                            {currentUser && (
-                              <div>
-                                <h2>{currentUser.username}</h2>
-                                <h2>{currentUser.password}</h2>
-                              </div>
-                            )}
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     )
 }
