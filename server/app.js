30860a25439b3f105be3b8f96452530238363449
zhugelijuan123 <nancyrain2015@163.com>
2023-04-15 18:39:38 -0700

    follows function

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-//import FollowsController from "./follows/follows-controller.js";
+import FollowsController from "./follows/follows-controller.js";
 import UsersController from "./users/users-controller.js";
 import ProductsController from "./products/products-controller.js";
 import mongoose from "mongoose";
@@ -75,6 +75,6 @@ app.get("/", function (req, res) {
 
 UsersController(app);
 ProductsController(app);
-//FollowsController(app);
+FollowsController(app);
 
 app.listen(process.env.PORT || 4000);
