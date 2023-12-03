8fcf451590cf6c3d1e4018f7c15d56738d3232d5
zhugelijuan123 <nancyrain2015@163.com>
2023-12-02 22:02:44 -0800

    adjust home page

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import ProductsList from "../components/ProductsList";
 import {Link} from "react-router-dom";
 import {fullTextSearch} from "../services/products/products-service";
-import {createProductThunk} from "../services/products/products-thunks";
+import {createProductThunk, findProductsThunk} from "../services/products/products-thunks";
 import {useDispatch} from "react-redux";
 import {useNavigate, useParams} from "react-router";
 
@@ -22,21 +22,35 @@ const HomeScreen = () => {
         return payload;
     }
 
+    const findProducts = async () => {
+            const [findResponse] = await Promise.all([
+                dispatch(findProductsThunk()),
+            ]);
+            const {payload} = findResponse;
+            return payload;
+    }
+
     const searchPunk = async () => {
         const query = search === "" ? "" : "beer_name=" + search;
-        let response = await fullTextSearch(query);
-        for (let i = 0; i < response.length; i++) {
-            response[i] = {
-                product_id: response[i].id,
-                name: response[i].name,
-                description: response[i].description,
-                image_url: response[i].image_url,
-                price: 9.99,
-                first_brewed: response[i].first_brewed,
-            }
-            response[i] = await saveProductsToDBAndReturn(response[i]);
+        if(!search){
+            let response = await findProducts();
+            setResults(response);
         }
-        setResults(response);
+        else{
+            let response = await fullTextSearch(query);
+            for (let i = 0; i < response.length; i++) {
+                response[i] = {
+                    product_id: response[i].id,
+                    name: response[i].name,
+                    description: response[i].description,
+                    image_url: response[i].image_url,
+                    price: 9.99,
+                    first_brewed: response[i].first_brewed,
+                }
+                response[i] = await saveProductsToDBAndReturn(response[i]);
+            }
+            setResults(response);
+        }
         if (!search) {
             navigate("/home");
         } else {
